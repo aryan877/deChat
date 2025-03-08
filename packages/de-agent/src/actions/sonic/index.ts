94a393eb8e6fc00ddc5a77b6584aef45aa3c5fad
aryan877 <aryankumar877@gmail.com>
2025-03-09 01:55:06 +0530

    refactor: remove native bridge

--- packages/de-agent/src/actions/sonic/index.ts
+++ packages/de-agent/src/actions/sonic/index.ts
@@ -9,12 +9,6 @@ import { getTransactionStatusAction } from "./getTransactionStatus.js";
 import { sonicTransferAction } from "./transfer.js";
 import { getStakersAction } from "./getStakers.js";
 import { getTokenBalanceAction } from "./getTokenBalance.js";
-import {
-  bridgeToSonicAction,
-  claimOnSonicAction,
-  bridgeToEthereumAction,
-  claimOnEthereumAction,
-} from "./nativeBridge.js";
 
 // Export individual actions
 export { getAccountInfoAction } from "./getAccountInfo.js";
@@ -23,12 +17,6 @@ export { getTokenSupplyAction } from "./getTokenSupply.js";
 export { getTransactionStatusAction } from "./getTransactionStatus.js";
 export { getStakersAction } from "./getStakers.js";
 export { getTokenBalanceAction } from "./getTokenBalance.js";
-export {
-  bridgeToSonicAction,
-  claimOnSonicAction,
-  bridgeToEthereumAction,
-  claimOnEthereumAction,
-} from "./nativeBridge.js";
 
 // Export all actions
 export const sonicActions: Record<string, Action> = {
@@ -39,8 +27,4 @@ export const sonicActions: Record<string, Action> = {
   [ACTION_NAMES.SONIC_TRANSFER]: sonicTransferAction,
   [ACTION_NAMES.SONIC_GET_STAKERS]: getStakersAction,
   [ACTION_NAMES.SONIC_GET_TOKEN_BALANCE]: getTokenBalanceAction,
-  [ACTION_NAMES.SONIC_BRIDGE_TO_SONIC]: bridgeToSonicAction,
-  [ACTION_NAMES.SONIC_CLAIM_ON_SONIC]: claimOnSonicAction,
-  [ACTION_NAMES.SONIC_BRIDGE_TO_ETHEREUM]: bridgeToEthereumAction,
-  [ACTION_NAMES.SONIC_CLAIM_ON_ETHEREUM]: claimOnEthereumAction,
 } as const;
